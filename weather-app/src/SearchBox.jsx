5668a65fa9743d933200af2a66c3bf45d956f0b4
Gautam <igautamji80gmail.com>
2025-12-11 16:05:51 +0530

    i am implement of the weather information code like temp humility max temp

--- weather-app/src/SearchBox.jsx
+++ weather-app/src/SearchBox.jsx
@@ -5,7 +5,25 @@ import { useState } from 'react';
 
 
 export default function SearchBox(){
-let [city, setCity] = useState("");
+ let [city, setCity] = useState("");
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY  = "bf01b2a6390978e9418d1e0d5d7afa9b";
+
+let getWeatherInfo = async () =>{
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+    let result = {
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feels_Like,
+        weather: jsonResponse.weather[0].description ,
+    }
+    console.log(result);
+}
+
 
 let handleChange = (eve) =>{
     setCity(eve.target.value);
@@ -16,6 +34,7 @@ let handleSumbit = (eve) =>{
     eve.preventDefault();
     console.log(city);
     setCity("");
+    getWeatherInfo();
 }
 
 
@@ -24,6 +43,7 @@ let handleSumbit = (eve) =>{
             <h3>Search for the weather </h3>
             <form onSubmit={handleSumbit}>
                 <TextField onChange={handleChange} value={city} id="outlined-basic" label="City Name" variant="outlined" />
+            <br/>
             <br/>
                 <Button variant="contained" type='sumbit'>Search</Button>
      
